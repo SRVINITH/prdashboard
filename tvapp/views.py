8aae5dbdb22369722e2f8f2115767b997e80c91d
koushikromel <koushikromel@gmail.com>
2023-02-28 16:33:32 +0530

    added try except

--- tvapp/views.py
+++ tvapp/views.py
@@ -9,20 +9,22 @@ def home(request):
     greenRoomHum = 0
     garageRoomTemp = 0
     garageRoomHum = 0
-    a = requests.get("http://192.168.1.16:8182/temp").json()
-    for i in a:
-        if i["room"] == "admin room":
-            adminRoomTemp = i["temperature"]
-            adminRoomHum = i["humidity"]
+    try:
+        a = requests.get("http://192.168.1.16:8182/temp").json()
+        for i in a:
+            if i["room"] == "admin room":
+                adminRoomTemp = i["temperature"]
+                adminRoomHum = i["humidity"]
 
-        if i["room"] == "green room":
-            greenRoomTemp = i["temperature"]
-            greenRoomHum = i["humidity"]
+            if i["room"] == "green room":
+                greenRoomTemp = i["temperature"]
+                greenRoomHum = i["humidity"]
 
-        if i["room"] == "garage":
-            garageRoomTemp = i["temperature"]
-            garageRoomHum = i["humidity"]
-
+            if i["room"] == "garage":
+                garageRoomTemp = i["temperature"]
+                garageRoomHum = i["humidity"]
+    except:
+        pass
     context = {
         "adminRoomTemp": adminRoomTemp,
         "adminRoomHum": adminRoomHum,
