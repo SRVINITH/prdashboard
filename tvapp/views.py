521d18fb667b42283af0afef097c318375f6d56c
koushikromel <koushikromel@gmail.com>
2023-03-01 19:54:56 +0530

    a

--- tvapp/views.py
+++ tvapp/views.py
@@ -17,21 +17,22 @@ def ajax(request):
     garageRoomTemp = 0
     garageRoomHum = 0
     try:
-        a = requests.get("http://192.168.1.16:8182/temp").json()
-        for i in a:
-            if i["room"] == "admin room":
-                adminRoomTemp = i["temperature"]
-                adminRoomHum = i["humidity"]
-
-            if i["room"] == "green room":
-                greenRoomTemp = i["temperature"]
-                greenRoomHum = i["humidity"]
-
-            if i["room"] == "garage":
-                garageRoomTemp = i["temperature"]
-                garageRoomHum = i["humidity"]
+        a = requests.get("http://192.168.1.16:8182/temp").json() #static ip
     except:
-        pass
+        a = requests.get("http://192.168.1.16:8182/temp").json() # local ip
+
+    for i in a:
+        if i["room"] == "admin room":
+            adminRoomTemp = i["temperature"]
+            adminRoomHum = i["humidity"]
+
+        if i["room"] == "green room":
+            greenRoomTemp = i["temperature"]
+            greenRoomHum = i["humidity"]
+
+        if i["room"] == "garage":
+            garageRoomTemp = i["temperature"]
+            garageRoomHum = i["humidity"]
     context = {
         "adminRoomTemp": adminRoomTemp,
         "adminRoomHum": adminRoomHum,
