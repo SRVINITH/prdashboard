483ab83cf6a427f1a7e8050d83f46abe8950c96e
sanjeev929 <sanjeevsanju929@gmail.com>
2023-03-10 12:08:11 +0530

    last update for fetch

--- tvapp/urls.py
+++ tvapp/urls.py
@@ -3,5 +3,5 @@ from .import views
 
 urlpatterns=[
     path('',views.home,name='home'),
-    # path("ajax/", views.ajax, name="ajax"),
+    path("ajax/", views.ajax, name="ajax"),
 ]
--- tvapp/views.py
+++ tvapp/views.py
@@ -21,7 +21,7 @@ db = firebase.database()
 
 
 def home(request):
-    time= datetime.now().strftime("%X")
+    time= datetime.now().strftime("%X%p")
     curr_day = datetime.now().strftime("%d")
     curr_mon_year = datetime.now().strftime("%Y-%m")
     curr_year = datetime.now().strftime("%Y")
@@ -78,64 +78,64 @@ def home(request):
     return render(request,'home.html',{'newleads':newleads,'half':half,'totalpoints':totalpoints,'monthname':curr_monthname,
     'currentdate':curr_date1,'time':time,'quotationcount':quotationcount,'invoicecount':invoicecount,'staffnamelist':staffnamelist})
 
-# def ajax(request):
-#     newYorkTz = pytz.timezone("Asia/Kolkata") 
-#     timeInNewYork = datetime.now(newYorkTz)
-#     currentTime = timeInNewYork.strftime("%-I:%M %p")
-#     currentDate = datetime.now().strftime("%d.%m.%Y")
-#     adminRoomTemp = 0
-#     adminRoomHum = 0
-#     greenRoomTemp = 0
-#     greenRoomHum = 0
-#     garageRoomTemp = 0
-#     garageRoomHum = 0
-#     quotationCount = 0
-#     invoiceCount = 0
-#     try:
-#         a = requests.get("http://192.168.1.18:8182/temp").json() #static ip
-#     except:
-#         a = requests.get("http://192.168.1.18:8182/temp").json() # local ip
-#     for i in a:
-#         if i["room"] == "Admin Room":
-#             adminRoomTemp = i["temperature"]
-#             adminRoomHum = i["humidity"]
+def ajax(request):
+    asiaTime = pytz.timezone("Asia/Kolkata")
+    asiaTime = datetime.now(asiaTime)
+    currentTime =  asiaTime.strftime("%I:%M:%S %p")
+    currentDate = datetime.now().strftime("%d.%m.%Y")
+    adminRoomTemp = 0
+    adminRoomHum = 0
+    greenRoomTemp = 0
+    greenRoomHum = 0
+    garageRoomTemp = 0
+    garageRoomHum = 0
+    quotationCount = 0
+    invoiceCount = 0
+    try:
+        a = requests.get("http://192.168.1.18:8182/temp").json() #static ip
+    except:
+        a = requests.get("http://192.168.1.18:8182/temp").json() # local ip
+    for i in a:
+        if i["room"] == "Admin Room":
+            adminRoomTemp = i["temperature"]
+            adminRoomHum = i["humidity"]
 
-#         if i["room"] == "Green Room":
-#             greenRoomTemp = i["temperature"]
-#             greenRoomHum = i["humidity"]
+        if i["room"] == "Green Room":
+            greenRoomTemp = i["temperature"]
+            greenRoomHum = i["humidity"]
 
-#         if i["room"] == "Garage":
-#             garageRoomTemp = i["temperature"]
-#             garageRoomHum = i["humidity"]
-#     quote = db.child("QuotationAndInvoice").get().val()
-#     tm = datetime.now()
-#     thisYear = tm.strftime("%Y")
-#     thisMonth = tm.strftime("%m")
-#     for i in quote:
-#         if i == "QUOTATION":
-#             try:
-#                 for q in quote[i][thisYear][thisMonth]:
-#                     quotationCount += 1
-#             except:
-#                 pass
-#         if i == "INVOICE":
-#             try:
-#                 for q in quote[i][thisYear][thisMonth]:
-#                     invoiceCount += 1
-#             except:
-#                 pass
+        if i["room"] == "Garage":
+            garageRoomTemp = i["temperature"]
+            garageRoomHum = i["humidity"]
+    quote = db.child("QuotationAndInvoice").get().val()
+    tm = datetime.now()
+    thisYear = tm.strftime("%Y")
+    thisMonth = tm.strftime("%m")
+    for i in quote:
+        if i == "QUOTATION":
+            try:
+                for q in quote[i][thisYear][thisMonth]:
+                    quotationCount += 1
+            except:
+                pass
+        if i == "INVOICE":
+            try:
+                for q in quote[i][thisYear][thisMonth]:
+                    invoiceCount += 1
+            except:
+                pass
 
-#     context = {
-#         "currentTime": currentTime,
-#         "currentDate": currentDate,
-#         "adminRoomTemp": adminRoomTemp,
-#         "adminRoomHum": adminRoomHum,
-#         "greenRoomTemp": greenRoomTemp,
-#         "greenRoomHum": greenRoomHum,
-#         "garageRoomTemp": garageRoomTemp,
-#         "garageRoomHum": garageRoomHum,
-#         "quotationCount": quotationCount,
-#         "invoiceCount": invoiceCount,
-#     }
-#     return JsonResponse(context)
+    context = {
+        "currentTime": currentTime,
+        "currentDate": currentDate,
+        "adminRoomTemp": adminRoomTemp,
+        "adminRoomHum": adminRoomHum,
+        "greenRoomTemp": greenRoomTemp,
+        "greenRoomHum": greenRoomHum,
+        "garageRoomTemp": garageRoomTemp,
+        "garageRoomHum": garageRoomHum,
+        "quotationCount": quotationCount,
+        "invoiceCount": invoiceCount,
+    }
+    return JsonResponse(context)
     
